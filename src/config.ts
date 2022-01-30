3827c9b010125ea40e0c2f2768e2fbb5015a49fe
Yest Izit <hi@dmitry.email>
2022-01-30 22:29:19 +0300

    fix: typo in tickrate multiplier

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const STARTING_HEAD_POSITION: Position = [6, 6]
 export const STARTING_SNAKE_SIZE = 1
 export const STARTING_TICKRATE = 500
 export const MIN_TICKRATE = 80
-export const TICKRATE_MULTIPLIER = 0.96
+export const TICKRATE_MULTIPLIER = 0.95
 
 export const LOCALSTORAGE_MAX_SCORE_KEY = "max-score"
 
